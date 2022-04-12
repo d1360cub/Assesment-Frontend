b6d526e41bc8c9b17c5590618bcf8bbdba02380c
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-04-12 17:00:17 -0500

    adds loading message

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -5,25 +5,31 @@ import './Grid.css';
 
 function Grid() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
   const getProducts = async () => {
     const data = await getAllProducts();
     setProducts(data);
+    setLoading(false);
   };
   useEffect(() => { getProducts(); }, []);
   return (
     <div>
-      <ul className="grid">
-        {
-          products.map((element) => (
-            <ProductCard
-              key={element.id}
-              id={element.id}
-              title={element.title}
-              image={element.image}
-            />
-          ))
+      {(loading) ? (
+        <div className="loading__text"><strong>I will be with you in a few seconds...</strong></div>
+      ) : (
+        <ul className="grid">
+          {
+            products.map((element) => (
+              <ProductCard
+                key={element.id}
+                id={element.id}
+                title={element.title}
+                image={element.image}
+              />
+            ))
           }
-      </ul>
+        </ul>
+      )}
     </div>
   );
 }
--- src/components/ProductDetails/index.jsx
+++ src/components/ProductDetails/index.jsx
@@ -6,38 +6,46 @@ import { getOneProduct } from '../../services/products';
 function ProductDetails() {
   const { id } = useParams();
   const [product, setProduct] = useState([]);
+  const [loading, setLoading] = useState(true);
   const getProduct = async () => {
     const data = await getOneProduct(id);
     setProduct(data);
+    setLoading(false);
   };
   useEffect(() => { getProduct(); }, []);
   const {
     title, price, description, category, image,
   } = product;
   return (
-    <div className="details__container">
-      <div className="details__title">{title}</div>
-      <img
-        src={image}
-        alt={title}
-        className="details__image"
-      />
-      <div>
-        <div className="details__price">
-          <strong>Precio (USD): </strong>
-          {price}
+    <div>
+      {(loading) ? (
+        <div className="loading__text"><strong>I will be with you in a few seconds...</strong></div>
+      ) : (
+        <div className="details__container">
+          <div className="details__title">{title}</div>
+          <img
+            src={image}
+            alt={title}
+            className="details__image"
+          />
+          <div>
+            <div className="details__price">
+              <strong>Precio (USD): </strong>
+              {price}
+            </div>
+            <ul className="details_list">
+              <li>
+                <strong>Categoría: </strong>
+                {category}
+              </li>
+              <li>
+                <strong>Descripción: </strong>
+                {description}
+              </li>
+            </ul>
+          </div>
         </div>
-        <ul className="details_list">
-          <li>
-            <strong>Categoría: </strong>
-            {category}
-          </li>
-          <li>
-            <strong>Descripción: </strong>
-            {description}
-          </li>
-        </ul>
-      </div>
+      )}
     </div>
   );
 }
