dcac717bf282f342fad73bcef4da881c7f15ebd9
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-04-12 08:08:27 -0500

    adds countdown

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+import { React, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Countdown from 'react-countdown';
 import './ProductCard.css';
 
 function ProductCard(props) {
   const { id, title, image } = props;
+  const [countdown, setCountdown] = useState(true);
+  const [buttonName, setButtonName] = useState('Product detail');
+  const handlerIsCompleted = () => {
+    setCountdown(false);
+    setButtonName('Offer expired');
+  };
   return (
     <li className="card__container">
       <img
@@ -14,8 +22,18 @@ function ProductCard(props) {
       />
       <div className="card__title">{title}</div>
       <div className="card__bottom">
-        <div className="card__timer">Timer</div>
-        <Link to={`/productdetails/${id}`} className="card__detail">Product detail</Link>
+        <div className="card__timer">
+          <Countdown
+            date={Date.now() + parseInt((Math.random() * (300000)), 10)}
+            onComplete={handlerIsCompleted}
+          />
+        </div>
+        <Link
+          className={countdown ? 'link__active' : 'link__inactive'}
+          to={`/productdetails/${id}`}
+        >
+          {buttonName}
+        </Link>
       </div>
     </li>
   );
